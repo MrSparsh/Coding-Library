d283a2600d521fde36b556b27730c5e95b32fb29
MrSparsh <mr.sparsh13@gmail.com>
2020-07-31 11:09:06 +0530

    loops corrected

--- template.cpp
+++ template.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define loop(i, k, n) for (ll i = k; i < n; i += 1)
-#define loop2(i, k, n, diff) for (ll i = k; i < n; i += diff)
+#define loop(i, k, n) for (ll i = k; i <= n; i += 1)
+#define loop2(i, k, n, diff) for (ll i = k; i <= n; i += diff)
 #define boost ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define pb push_back
 typedef long long ll; typedef pair<ll, ll> pl; typedef vector<ll> vl; typedef vector<pl> vpl; typedef vector<vl> vvl; typedef vector<vpl> vvpl;
 const ll mod = 1000000007;
 
 void solve(int tc){
-
+	
 }
 
 int main()
