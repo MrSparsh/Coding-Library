2f64ad0a5c3143c3eb47290a38f26e3bb2990555
MrSparsh <mr.sparsh13@gmail.com>
2020-07-31 10:59:25 +0530

    Template Improved

--- template.cpp
+++ template.cpp
@@ -1,15 +1,15 @@
-//My template for solving general problems
-
 #include <bits/stdc++.h>
 using namespace std;
 #define loop(i, k, n) for (ll i = k; i < n; i += 1)
 #define loop2(i, k, n, diff) for (ll i = k; i < n; i += diff)
-#define debug2(x, y) cout << #x << " :: " << x << "\t" << #y << " :: " << y << endl;
-#define debug3(x, y, z) cout << #x << " :: " << x << "\t" << #y << " :: " << y << "\t" << #z << " :: " << z << endl;
 #define boost ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define pb push_back
 typedef long long ll; typedef pair<ll, ll> pl; typedef vector<ll> vl; typedef vector<pl> vpl; typedef vector<vl> vvl; typedef vector<vpl> vvpl;
 const ll mod = 1000000007;
+
+void solve(int tc){
+
+}
 
 int main()
 {
@@ -17,7 +17,10 @@ int main()
 	#ifndef ONLINE_JUDGE
 		freopen("input.txt", "r", stdin);
 	#endif
-
-	
+	int t=1;
+	cin>>t;
+	loop(tc,1,t){
+		solve(tc);
+	}
 	return 0;
 }
